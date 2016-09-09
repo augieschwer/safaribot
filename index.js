c84a72975b3c5d315f8ecc2da4e73ae79ce5da59
Augie Schwer <augie@safaribooksonline.com>
2016-09-09 11:37:53 -0700

    Allow for debug messages. Our first ping/pong message.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ if (!process.env.token) {
 }
 
 var Botkit = require('botkit');
-var controller = Botkit.slackbot();
+var controller = Botkit.slackbot(
+    { debug: process.env.debug }
+);
 var bot = controller.spawn({
       token: process.env.token
 })
@@ -12,4 +14,8 @@ bot.startRTM(function(err,bot,payload) {
       if (err) {
               throw new Error('Could not connect to Slack');
                 }
+});
+
+controller.hears(['ping'], ['direct_message','direct_mention'], function(bot,message) {
+    bot.reply(message,'PONG');
 });
