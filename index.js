6488549a0d0d7828a9af745c949d01138e28b020
Augie Schwer <augie@safaribooksonline.com>
2016-09-09 13:04:22 -0700

    Include mentions too, like 'hey @safaribot'.

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ bot.startRTM(function(err,bot,payload) {
                 }
 });
 
-controller.hears(['ping'], ['direct_message','direct_mention'], function(bot,message) {
+controller.hears(['ping'], ['direct_message','direct_mention','mention'], function(bot,message) {
     bot.reply(message,'PONG');
 });
 
-controller.hears(['hello','hi'], ['direct_message','direct_mention'], function(bot,message) {
+controller.hears(['hello','hi'], ['direct_message','direct_mention','mention'], function(bot,message) {
     bot.startConversation(message, function(err, convo) {
         convo.say('Howdy!');
     });
