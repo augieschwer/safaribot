3350f36f0ecf03053580b8f2a266c48024b545b3
Augie Schwer <augie@safaribooksonline.com>
2016-09-23 10:36:51 -0700

    store the response topic from the user in a var, and use that var, also offer a more focused URL if the first one was not helpful

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ controller.hears(['ping'], ['direct_message','direct_mention','mention'], functi
 controller.on(['direct_message','direct_mention','mention'], function(bot,message) {
     bot.startConversation(message, function(err, convo) {
         convo.ask('Howdy! What would you like to learn about?', function(response,convo) {
-            convo.say('Cool! '+ response.text + ' sounds interesting');
-            convo.say('Here is a link to some content about ' + response.text + ': https://www.safaribooksonline.com/search/?query=' + response.text);
+            var topic = response.text;
+            convo.say('Cool! '+ topic + ' sounds interesting');
+            convo.say('Here is a link to some content about ' + topic + ': https://www.safaribooksonline.com/search/?query=' + topic);
             convo.ask('Was that helpful?', [
                 {
                     pattern: bot.utterances.yes,
@@ -37,6 +38,7 @@ controller.on(['direct_message','direct_mention','mention'], function(bot,messag
                     pattern: bot.utterances.no,
                     callback: function(response,convo) {
                         convo.say('Bummer...');
+                        convo.say('Maybe this link will be more helpful: https://www.safaribooksonline.com/search/?query=' + topic + '&field=title');
                         convo.next();
                     }
                 },
